38c66c72488c42a6892449a168e3eee14c951c24
Caprico1 <suprn8@live.com>
2018-04-16 16:36:12 -0500

    initial idea of the dodge mechanic is in place. Animator and mannequin model is needed to test.

---
+++ Assets/Scripts/PlayerDodge.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDodge : MonoBehaviour {
+
+    //Will be used when animation is used.
+    public Animator animator;
+    public GameObject player;
+    public int distance;
+    private Vector3 current_position;
+    private bool is_rolling;
+
+    // Use this for initialization
+    void Start() {
+
+        if (!player)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        current_position = player.transform.position;
+        is_rolling = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetAxisRaw("Dodge") == 1 && is_rolling == false)
+        {
+            StartCoroutine(Roll());
+        }
+    }
+
+    IEnumerator Roll(){
+        current_position.z += distance;
+        player.transform.Translate(current_position * Time.deltaTime);
+        is_rolling = true;
+        yield return new WaitForSeconds(1f);
+        StopCoroutine(Roll());
+        is_rolling = false;
+    }
+}
