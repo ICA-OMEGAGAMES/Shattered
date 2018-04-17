2793637396bf5b360783c74ce72ae6d15554b116
Caprico1 <suprn8@live.com>
2018-04-17 13:51:17 -0500

    animations kinda work now. They need to be integrated with the player animation controller.

---
+++ Assets/Scripts/PlayerCrouch.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCrouch : MonoBehaviour {
+
+    public GameObject player;
+    public Animator anim;
+
+    private int crouch;
+    private int idle;
+    private int crouch_walk;
+    private bool crouched;
+
+	// Use this for initialization
+	void Start () {
+	    if (!player)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }	
+
+        if (!anim)
+        {
+            anim = player.GetComponent<Animator>();
+        }
+
+        crouch = Animator.StringToHash("Crouch");
+        idle = Animator.StringToHash("Idle");
+        crouch_walk = Animator.StringToHash("crouched_walk");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxisRaw("Crouch") == 1)
+        {
+            Debug.Log("crouching");
+            anim.CrossFade(crouch, .01f);
+            crouched = true;
+        }
+        else
+        {
+            crouched = false;
+            anim.CrossFade(idle, .01f);
+        }
+
+        if (Input.GetAxisRaw("Vertical") !=0 && crouched == true)
+        {
+            anim.CrossFade(crouch_walk, .5f);
+        }
+	}
+}
