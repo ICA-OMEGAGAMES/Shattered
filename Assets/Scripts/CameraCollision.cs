57bf108f6e317649a924f34b383968536268b5da
BenjaminFHV <bgr4078@students.fhv.at>
2018-04-17 13:59:50 +0200

    thirdPersonCamera - implemented & tested (settings with Scriptable Objects)

---
+++ Assets/Scripts/CameraCollision.cs
@@ -0,0 +1,44 @@
+﻿//-------------------------------------------------------------------------
+// Author:			Benjamin Grabherr                    Date: 17.04.2018
+// Description:		This is a camera Collision Script in
+// 					a third person view. If the player-camera
+//					hit an object, the cam will automaticly 
+//					go near the player.
+// Source:			https://www.youtube.com/watch?v=LbDQHv9z-F0&t=127s
+//
+//-------------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollision : MonoBehaviour
+{
+	[SerializeField] CameraCollisionSetting myCollisionData;
+    private Vector3 dollyDir;
+
+    // Use this for initialization
+    void Awake()
+    {
+        dollyDir = transform.localPosition.normalized;
+        myCollisionData.distance = transform.localPosition.magnitude;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 desiredCameraPos = transform.parent.TransformPoint(dollyDir * myCollisionData.maxDistance);
+        RaycastHit hit;
+
+        if (Physics.Linecast(transform.parent.position, desiredCameraPos, out hit))
+        {
+            myCollisionData.distance = Mathf.Clamp((hit.distance * 0.87f), myCollisionData.minDistance, myCollisionData.maxDistance);
+        }
+        else
+        {
+            myCollisionData.distance = myCollisionData.maxDistance;
+        }
+
+        transform.localPosition = Vector3.Lerp(transform.localPosition, dollyDir * myCollisionData.distance, Time.deltaTime * myCollisionData.smooth);
+    }
+}
---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,79 @@
+﻿//-------------------------------------------------------------------------
+// Author:			Benjamin Grabherr                    Date: 16.04.2018
+// Description:		This is a camera Follow Script in
+// 					a third person view.
+// Source:			https://www.youtube.com/watch?v=LbDQHv9z-F0&t=127s
+//
+//-------------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	[SerializeField] CameraFollowSetting myFollowData;
+    [SerializeField] GameObject cameraFollowObj;
+    // [SerializeField] GameObject cameraObj;
+    // [SerializeField] GameObject playerObj;
+    private Vector3 followPOS;
+	private float rotY = 0.0f;
+    private float rotX = 0.0f;
+
+    // public float smoothX;
+    // public float smoothY;
+	// public float camDistanceXToPlayer;
+    // public float camDistanceYToPlayer;
+    // public float camDistanceZToPlayer;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+
+        // Block the cursor & make him unvisible
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Setup the rotation of the sticks here --> Supports also the Controller
+        float inputX = Input.GetAxis("RightStickHorizontal");
+        float inputZ = Input.GetAxis("RightStickVertical");
+        myFollowData.mouseX = Input.GetAxis("Mouse X");
+        myFollowData.mouseY = Input.GetAxis("Mouse Y");
+        myFollowData.finalInputX = inputX + myFollowData.mouseX;
+        myFollowData.finalInputZ = inputZ + myFollowData.mouseY;
+
+        // Rotate the stick, depending where we pushing
+        rotY += myFollowData.finalInputX * myFollowData.inputSensitivity * Time.deltaTime;
+        rotX += myFollowData.finalInputZ * myFollowData.inputSensitivity * Time.deltaTime;
+
+        // Clamp that value, so it can't go higher or lower --> stop it from going around and around in circles
+        rotX = Mathf.Clamp(rotX, -myFollowData.clampAngle, myFollowData.clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+    }
+
+    void LateUpdate()
+    {
+        CameraUpdater();
+    }
+
+    private void CameraUpdater()
+    {
+        // set the target object to follow
+        Transform target = cameraFollowObj.transform;
+
+        // move towards the game object that is the target
+        float step = myFollowData.cameraMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
