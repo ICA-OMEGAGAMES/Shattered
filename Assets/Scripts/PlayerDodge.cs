f456e8ef35cd1a37954e48885c173dba56566c5c
Caprico1 <suprn8@live.com>
2018-04-17 11:44:03 -0500

    more animations and modifications to the dodge script. the player can now dodge left, right, forward, and back. Animations are next.

--- Assets/Scripts/PlayerDodge.cs
+++ Assets/Scripts/PlayerDodge.cs
@@ -9,7 +9,15 @@ public class PlayerDodge : MonoBehaviour {
     public GameObject player;
     public int distance;
     private Vector3 current_position;
+    
+    //Bool to check if the player is rolling.
     private bool is_rolling;
+
+    //helper to track the axis.
+    private string axis;
+
+    //Store either 1 or -1 for the positive or negative direction of the player's input.
+    private int direction;
 
     // Use this for initialization
     void Start() {
@@ -24,15 +32,53 @@ public class PlayerDodge : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update() {
-        if (Input.GetAxisRaw("Dodge") == 1 && is_rolling == false)
+    void Update()
+    {
+        
+        // Forward dodge check
+        if (Input.GetAxisRaw("Dodge") == 1 && is_rolling == false && Input.GetAxisRaw("Vertical") == 1)
         {
+            axis = "z";
+            direction = 1;
+            StartCoroutine(Roll());
+        }
+        //Backward dodge check
+        else if (Input.GetAxisRaw("Dodge") == 1 && is_rolling == false && Input.GetAxisRaw("Vertical") == -1)
+        {
+            axis = "z";
+            direction = -1;
+            StartCoroutine(Roll());
+        }
+        //Right dodge/step check
+        else if (Input.GetAxisRaw("Dodge") == 1 && is_rolling == false && Input.GetAxisRaw("Horizontal") == 1)
+        {
+            axis = "x";
+            direction = 1;
+            StartCoroutine(Roll());
+        }
+        //Left dodge/step check
+        else if (Input.GetAxisRaw("Dodge") == 1 && is_rolling == false && Input.GetAxisRaw("Horizontal") == -1)
+        {
+            axis = "x";
+            direction = -1;
             StartCoroutine(Roll());
         }
     }
 
+    //Dodge Coroutine
     IEnumerator Roll(){
-        current_position.z += distance;
+
+        Debug.Log("Axis: " + axis);
+        Debug.Log("direction: " + direction);
+
+        if (axis == "x")
+        {
+            current_position.x += distance * direction;
+        }
+        else if (axis == "z")
+        {
+            current_position.z += distance * direction;
+        }
         player.transform.Translate(current_position * Time.deltaTime);
         is_rolling = true;
         yield return new WaitForSeconds(1f);
