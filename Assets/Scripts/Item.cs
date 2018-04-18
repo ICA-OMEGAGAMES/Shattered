883337c0c2b1f0cd8e4627f165ce2f640f2c35f3
Zoe Kroontje-Gillette <z.gillette@hotmail.com>
2018-04-18 15:37:15 +0200

    Adds the option to examine items with the 'e' (Examine button). Also adds a controller to give items a scriptable object with a name, description, interactable boolean and image.

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu (menuName = "Shattered/Item")]
+public class ItemScriptableObject : ScriptableObject {
+
+	public string name = "Item name.";
+	public string description = "Item description.";
+
+	public bool interactable = false;
+
+	public Image image = null;
+
+}
---
+++ Assets/Scripts/ItemController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemController : MonoBehaviour {
+	public ItemScriptableObject item;
+
+	public string GetName(){return item.name;}
+	public string GetDescription(){return item.description;}
+	public Image GetImage(){return item.image;}
+	public bool CanBePickedUp(){return item.interactable;}
+}
