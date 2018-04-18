3feadf2acc8b6354d902e5f0cdb0010ac544bbdd
Zoe Kroontje-Gillette <z.gillette@hotmail.com>
2018-04-18 14:47:56 +0200

    Adds the picking up of game objects tagged 'Item' using the 'f' Pickup button by usage of colliders.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float horizontal = Input.GetAxisRaw("Horizontal");
+		float vertical = Input.GetAxisRaw("Vertical");
+		rb.AddForce(new Vector3(horizontal, 0.0f, vertical) * 10);
+	}
+
+
+	void OnTriggerStay (Collider other)
+	{
+		if(Input.GetButtonDown("Pickup"))
+			if (other.gameObject.CompareTag ("Item"))
+				other.gameObject.SetActive (false);
+	}
+}
