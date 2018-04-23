aa4035c00bacdd0f4ccea569ffd8f88a4522fcf0
BenjaminFHV <bgr4078@students.fhv.at>
2018-04-23 13:14:14 +0200

    added YarnSpinnerScripts + DemoLevelScene for testing

---
+++ Assets/Scripts/YarnSpinner/Code/YarnCommands.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Yarn.Unity.Shattered
+{
+
+    public class YarnCommands : MonoBehaviour
+    {
+
+        [YarnCommand("doJump")]
+        public void jump(string jumpheight)
+        {
+            Debug.Log("Command --> doJump");
+
+            // simulate the Jump function
+            transform.position = new Vector3(0.0f, 1.5f, 0.0f);
+        }
+
+        [YarnCommand("startPressureTimer")]
+        public void pressureTimer(string test)
+        {
+            Debug.Log("Command --> startPressureTimer");
+
+            //We get all parameters and split them, so that we can use these later
+            var parameters = test.Split(',');
+            
+            // float seconds = (float) double.Parse(parameters[0]);
+            int definedOption = int.Parse(parameters[1]) - 1;
+            int seconds = int.Parse(parameters[0]);
+
+            // set the values, which we get from the command and put it into the PressureTimer
+            PressureTimer.Instance.isActive = true;
+            PressureTimer.Instance.time = seconds;
+
+            // Check if definedOption is lower than 0
+            if (definedOption < 0)
+            {
+                definedOption = 0;
+            }
+
+            PressureTimer.Instance.selectedOption = definedOption;
+        }
+
+        [YarnCommand("startHelpTimer")]
+        public void helpTimer(string time) {
+            Debug.Log("Command --> startHelpTimer");
+        }
+    }
+}
