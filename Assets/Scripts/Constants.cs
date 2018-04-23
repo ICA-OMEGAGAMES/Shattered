b293f1cdea50c4de1ae6ba242412dcf8a0513eb6
Zoe Kroontje-Gillette <z.gillette@hotmail.com>
2018-04-23 13:11:15 +0200

    health constant removal

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -15,7 +15,4 @@ public static class Constants{
 	public const string DODGE_BUTTON = "Dodge";
 	public const string JUMP_BUTTON = "Jump";
 
-	// SETTINGS
-	public const int PLAYER_HEALTH = 100;
-
 }
