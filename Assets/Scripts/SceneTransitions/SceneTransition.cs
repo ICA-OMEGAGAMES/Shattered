965b60cba4cf52a4bd3a6b3ab9c68a6363affea7
Caprico1 <suprn8@live.com>
2018-05-16 16:19:38 -0500

    Switch to scenes on trigger enter.

---
+++ Assets/Scripts/SceneTransitions/SceneTransition.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour {
+
+    //index of the current string
+    private int current_scene;
+
+	// Use this for initialization
+	void Start () {
+        int current_scene = SceneManager.GetActiveScene().buildIndex;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    //if (Input.GetButton("Pickup"))
+     //   {
+     //       SceneManager.LoadScene(next_scene);
+     //   }	
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(current_scene +1);
+        }
+    }
+
+
+
+
+}
