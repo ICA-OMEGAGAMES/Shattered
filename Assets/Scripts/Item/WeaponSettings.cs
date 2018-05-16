d9f309450e510f9374539755dbc59d1d1b6a3378
Zoe Kroontje-Gillette <z.gillette@hotmail.com>
2018-05-16 16:24:14 +0200

    no message

---
+++ Assets/Scripts/Item/WeaponSettings.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "Shattered/Weapon")]
+public class WeaponSettings : ItemSettings
+{
+    public float attack1Damage = 1;
+    public float attack1Duration = 0.5f;
+    public bool attack1Rootable = true;
+    public float attack2Damage = 1;
+    public float attack2Duration = 0.5f;
+    public bool attack2Rootable = true;
+    public DevonScript.FSMState weaponType = DevonScript.FSMState.LightMeleeWeapon;
+}
---
+++ Assets/Scripts/Main menu/GraphicsSettingsManager.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Manages selection of configurations, storing configurations and changing them
+public class GraphicsSettingsManager : MonoBehaviour {
+
+	public Toggle fullscreenToggle;
+	public Dropdown resolutionDropdown;
+	public Dropdown graphicsQualityDropdown;
+	public Dropdown textureQualityDropdown;
+	public Dropdown antialiasingDropdown;
+	public Dropdown vSyncDropdown;
+	public Button backButton;
+
+	private Resolution[] resolutions;
+
+	void OnEnable()
+	{
+		SetUpResolutions();
+		SetUpGraphicsQuality();
+		
+		fullscreenToggle.isOn = GraphicSettings.Instance.fullscreen = Screen.fullScreen;
+		textureQualityDropdown.value = GraphicSettings.Instance.textureQuality = QualitySettings.masterTextureLimit;
+		GraphicSettings.Instance.antialiasing = QualitySettings.antiAliasing;
+		antialiasingDropdown.value = (int)Mathf.Log(GraphicSettings.Instance.antialiasing, 2);
+		vSyncDropdown.value = GraphicSettings.Instance.vSync = QualitySettings.vSyncCount;
+		graphicsQualityDropdown.value = GraphicSettings.Instance.graphicsQuality = QualitySettings.GetQualityLevel();;
+		
+		if(graphicsQualityDropdown.value != 0)
+		{
+			textureQualityDropdown.interactable = false;
+			antialiasingDropdown.interactable = false;
+			vSyncDropdown.interactable = false;
+		}
+		
+		AddListeners();
+	}
+
+	public void OnFullScreenToggle(){
+		GraphicSettings.Instance.fullscreen = Screen.fullScreen = fullscreenToggle.isOn;
+	}
+
+	public void OnGraphicsQualityChange()
+	{
+		GraphicSettings.Instance.graphicsQuality = graphicsQualityDropdown.value;
+		QualitySettings.SetQualityLevel(GraphicSettings.Instance.graphicsQuality, true);
+
+		bool isEnabled = (GraphicSettings.Instance.graphicsQuality ==  0);
+		textureQualityDropdown.interactable = isEnabled;
+		antialiasingDropdown.interactable = isEnabled;
+		vSyncDropdown.interactable = isEnabled;
+		
+		SyncValues();
+	}
+
+	public void OnResolutionChange()
+	{
+		Screen.SetResolution(resolutions[resolutionDropdown.value].width, resolutions[resolutionDropdown.value].height, Screen.fullScreen);
+		GraphicSettings.Instance.resolutionIndex = resolutionDropdown.value;
+	}
+
+	public void OnTextureQualityChange()
+	{
+		GraphicSettings.Instance.textureQuality = QualitySettings.masterTextureLimit = textureQualityDropdown.value;
+	}
+
+	public void OnAntialiasingChange()
+	{
+		QualitySettings.antiAliasing = GraphicSettings.Instance.antialiasing = (int)Mathf.Pow(2, antialiasingDropdown.value);
+	}
+
+	public void OnVsyncChange()
+	{
+		GraphicSettings.Instance.vSync = QualitySettings.vSyncCount = vSyncDropdown.value;
+	}
+
+	private void AddListeners()
+	{
+		fullscreenToggle.onValueChanged.AddListener(delegate {OnFullScreenToggle();});
+		graphicsQualityDropdown.onValueChanged.AddListener(delegate {OnGraphicsQualityChange();});
+		resolutionDropdown.onValueChanged.AddListener(delegate {OnResolutionChange();});
+		textureQualityDropdown.onValueChanged.AddListener(delegate {OnTextureQualityChange();});
+		antialiasingDropdown.onValueChanged.AddListener(delegate {OnAntialiasingChange();});
+		vSyncDropdown.onValueChanged.AddListener(delegate {OnVsyncChange();});
+		backButton.onClick.AddListener(delegate{SaveSettings();});
+	}
+
+	private void SyncValues()
+	{
+		GraphicSettings.Instance.fullscreen = fullscreenToggle.isOn = Screen.fullScreen;
+		GraphicSettings.Instance.textureQuality = textureQualityDropdown.value = QualitySettings.masterTextureLimit;
+		GraphicSettings.Instance.antialiasing = QualitySettings.antiAliasing;
+		antialiasingDropdown.value = (int)Mathf.Log(GraphicSettings.Instance.antialiasing, 2);
+		GraphicSettings.Instance.vSync = vSyncDropdown.value = QualitySettings.vSyncCount;
+		GraphicSettings.Instance.graphicsQuality = graphicsQualityDropdown.value = QualitySettings.GetQualityLevel();
+	}
+
+	private void SetUpResolutions()
+	{
+		int counter = 0;
+		resolutions = Screen.resolutions;
+		resolutionDropdown.options.RemoveRange(0, resolutionDropdown.options.Count);
+		foreach(Resolution reso in resolutions)
+		{
+			resolutionDropdown.options.Add(new Dropdown.OptionData(reso.width + " x " + reso.height + " @" + reso.refreshRate + "Hz"));
+			if(reso.height.Equals(Screen.height) && reso.width.Equals(Screen.width))
+			{
+				GraphicSettings.Instance.resolutionIndex = resolutionDropdown.value = counter;
+			}
+			counter++;
+		}
+	}
+
+	private void SetUpGraphicsQuality()
+	{
+		string[] names = QualitySettings.names;
+		graphicsQualityDropdown.options.RemoveRange(0, graphicsQualityDropdown.options.Count);
+		foreach(string name in names)
+		{
+			graphicsQualityDropdown.options.Add(new Dropdown.OptionData(name));
+		}
+	}
+
+	//Saves current settings to a config file
+	public void SaveSettings()
+	{
+		string jsonData = JsonUtility.ToJson (GraphicSettings.Instance, true);
+		File.WriteAllText (Application.persistentDataPath + Constants.GRAPHICS_JSON, jsonData);
+	}
+
+}
+
+
+
+
