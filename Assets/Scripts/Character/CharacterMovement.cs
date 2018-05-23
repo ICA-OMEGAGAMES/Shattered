a31f81daf6eb6040de1e2a06a1275a7eb80d1101
Zoe Kroontje-Gillette <z.gillette@hotmail.com>
2018-05-23 14:38:09 +0200

    barge around and knock over bottles like the agressive shit you are

--- Assets/Scripts/Character/CharacterMovement.cs
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -325,4 +325,18 @@ public class CharacterMovement : MonoBehaviour
             this.combatState = value;
         }
     }
+
+	public float pushPower = 2.0f;
+	void OnControllerColliderHit(ControllerColliderHit hit){
+		Rigidbody body = hit.collider.attachedRigidbody;
+		if (body == null || body.isKinematic)
+			return;
+
+		if (hit.moveDirection.y < -0.3f)
+			return;
+
+		Vector3 pushDir = new Vector3 (hit.moveDirection.x, 0, hit.moveDirection.z);
+		body.velocity = pushDir * pushPower;
+	}
+
 }
