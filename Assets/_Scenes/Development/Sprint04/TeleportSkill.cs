93590da19d10aa8c382947d442d7d26e14259922
BenjaminFHV <bgr4078@students.fhv.at>
2018-05-28 14:46:43 +0200

    first draft, if we start teleport, the mouse will be visible

---
+++ Assets/_Scenes/Development/Sprint04/TeleportSkill.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportSkill : MonoBehaviour
+{
+    // public bool isActive;
+    public TimeManager timeManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        // isActive = false;
+        // this.transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton(Constants.SKILL3_BUTTON))
+        {
+            Debug.Log("StartSkill");
+            timeManager.DoSlowmotion();
+            startTeleportSkill();
+        }
+    }
+
+    void startTeleportSkill()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = this.transform.position;
+        }
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+        {
+            if (hit.transform.CompareTag("Floor"))
+            {
+                this.transform.position = hit.point;
+                this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 0.05f, this.transform.position.z);
+            }
+        }
+    }
+}
---
+++ Assets/_Scenes/Development/Sprint04/TimeManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class TimeManager : MonoBehaviour {
+
+	//Controll how much we slow down Time if we start the Manager
+	public float slowDownFactor = 0.05f;
+	//How long the slow down will occure maybee not needed
+	public float slowdownDuration = 2f;
+
+	void Update()
+	{
+		Time.timeScale += (1f / slowdownDuration) * Time.unscaledDeltaTime;
+		Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+	}
+
+	public void DoSlowmotion()
+	{
+		Debug.Log("DoSlowmotion");
+		Time.timeScale = slowDownFactor;
+		Time.fixedDeltaTime = Time.timeScale * 0.02f;
+	}
+	
+}
