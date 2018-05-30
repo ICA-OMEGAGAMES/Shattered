2334a2c8fa0a2e3f25b215e5766003484536a828
Zoe Kroontje-Gillette <z.gillette@hotmail.com>
2018-05-30 12:16:42 +0200

    Scrollzoom and billboard

---
+++ Assets/CameraZoom.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour {
+
+	float minFov = 15f;
+	float maxFov = 90f;
+	float sensitivity = 10f;
+
+	void Update () {
+		float fov = Camera.main.fieldOfView;
+		fov += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+		fov = Mathf.Clamp(fov, minFov, maxFov);
+		Camera.main.fieldOfView = fov;
+	}
+}
---
+++ Assets/Scripts/Item/Billboard.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+
+public class Billboard : MonoBehaviour
+{
+	public Camera main;
+
+	void Update()
+	{
+		transform.LookAt (transform.position + main.transform.rotation * Vector3.forward, main.transform.rotation * Vector3.up);
+	}
+}
