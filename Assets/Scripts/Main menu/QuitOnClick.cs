1fd5481b22da9eb31bf469086668445259ae4157
Julian Berchtold <jbe1917@students.fhv.at>
2018-05-30 13:54:49 +0200

    Revert "added  main and ingame menu"
    This reverts commit a35eee604fd828ec88d64669e9b9cbff27c590cf.

--- Assets/Scripts/Main menu/QuitOnClick.cs
+++ Assets/Scripts/Main menu/QuitOnClick.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 //Script to quit the game when quit is clicked
 public class QuitOnClick : MonoBehaviour {
 
-	private Button quitButton;
+	public Button quitButton;
 
 	void Start()
 	{
