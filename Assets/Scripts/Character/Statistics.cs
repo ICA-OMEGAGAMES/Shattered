be642d93dc1016315db7cbddb8adfbd81decad8a
Sander <Sandervanuum@hotmail.com>
2018-05-31 14:21:20 +0200

    fix, + prefab fixed

--- Assets/Scripts/Character/Statistics.cs
+++ Assets/Scripts/Character/Statistics.cs
@@ -8,6 +8,7 @@ public class Statistics : MonoBehaviour {
 	public Slider healthbar;
 
 	public float health;
+    public float hitStunDuration = 0.5f;
     private float maxHealth = 100f;
     private float blocks = 0;
     private bool immune = false;
@@ -33,6 +34,8 @@ public class Statistics : MonoBehaviour {
             {
                 if (blocking)
                     amount = amount -(amount * blockReductionPersentage);
+                else
+                    Stun(hitStunDuration);
                 health -= amount;
 
                 if (health <= Constants.MIN_PLAYER_HEALTH)
@@ -90,5 +93,18 @@ public class Statistics : MonoBehaviour {
     private void ActivateShield(bool activation)
     {
         shield.SetActive(activation);
+    }
+
+    private void Stun(float duration)
+    {
+        //find component call stunn
+        if (GetComponent<CharacterTransformer>().currentForm == CharacterTransformer.CharacterForm.devon)
+        {
+            StartCoroutine(GetComponentInChildren<DevonScript>().StunCharacter(duration));
+        }
+        else if (GetComponent<CharacterTransformer>().currentForm == CharacterTransformer.CharacterForm.malphas)
+        {
+            StartCoroutine(GetComponentInChildren<MalphasScript>().StunCharacter(duration));
+        }
     }
 }
