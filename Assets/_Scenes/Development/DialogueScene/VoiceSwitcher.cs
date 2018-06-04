06ddb63fb31a83adbbd6d57f5a629f08fb790eaf
BenjaminFHV <bgr4078@students.fhv.at>
2018-06-04 13:54:43 +0200

    added faces & Audi & PressureTimer

---
+++ Assets/_Scenes/Development/DialogueScene/VoiceSwitcher.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using Yarn.Unity.Shattered;
+using UnityEngine.UI;
+
+public class VoiceSwitcher : MonoBehaviour
+{
+    [System.Serializable]
+    public struct VoiceInfo
+    {
+        public string name;
+        public AudioClip voice;
+    }
+
+    public VoiceInfo[] voices;
+    private float duration;
+    private bool isRunning = false;
+    private IEnumerator coroutine;
+
+    void Start()
+    {
+        coroutine = WaitForSound();
+    }
+
+    [YarnCommand("setVoice")]
+    public void SetVoice(string voiceName)
+    {
+        if (isRunning)
+        {
+            StopCoroutine(coroutine);
+        }
+
+        AudioClip tempClip = null;
+
+        foreach (var info in voices)
+        {
+            if (info.name == voiceName)
+            {
+                tempClip = info.voice;
+                break;
+            }
+        }
+
+        if (tempClip == null)
+        {
+            Debug.LogErrorFormat("Can't find sprite named {0}!", voiceName);
+            return;
+        }
+
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.clip = tempClip;
+        audio.Play();
+        isRunning = true;
+
+        duration = tempClip.length;
+
+        StartCoroutine(coroutine);
+    }
+
+    IEnumerator WaitForSound()
+    {
+        yield return new WaitForSeconds(duration);
+        isRunning = false;
+    }
+}
