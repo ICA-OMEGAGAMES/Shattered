75e4b37d6a32a6095acee95cb908bd615d8af098
Zoe Kroontje-Gillette <z.gillette@hotmail.com>
2018-06-06 15:23:08 +0200

    Corruption and environment implementation

--- Assets/Scripts/Environment/RainController.cs
+++ Assets/Scripts/Environment/RainController.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class RainController : MonoBehaviour {
 
-	public ParticleSystem ps;
+	public EnvironmentSettings envSettings;
 
-	void Start (){
-		ps.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+	private Rain rain;
+	private Rain bloodRain;
+
+	void Awake(){
+		rain = GameObject.Find ("Regular").GetComponent<Rain> ();
+		bloodRain = GameObject.Find ("Blood").GetComponent<Rain> ();
 	}
 
-	void OnTriggerStay(Collider other){
-		ps.Play(true);
+	void Update(){
+		rain.setEmission (envSettings.rain.current);
+		if (envSettings.rain.current != 0) {
+			rain.PlayRainSideEffects ();
+
+			if (envSettings.rain.current >= envSettings.rain.maximumCorruption / 2)
+				rain.heavy.Play ();
+		} else {
+			rain.heavy.Stop ();
+			rain.StopRainSideEffects ();
+		}
+
+
+		bloodRain.setEmission (envSettings.bloodRain.current);
+		if (envSettings.bloodRain.current != 0) {
+			bloodRain.PlayRainSideEffects ();
+			if (envSettings.bloodRain.current >= envSettings.bloodRain.maximumCorruption / 2)
+				bloodRain.heavy.Play ();
+		} else{
+			bloodRain.StopRainSideEffects ();
+			bloodRain.heavy.Stop ();
+		}
+	
 	}
 
-	void OnTriggerExit(Collider other){
-		ps.Stop(true, ParticleSystemStopBehavior.StopEmitting);
-	}
+		
+
 }
