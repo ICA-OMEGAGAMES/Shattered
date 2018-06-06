4ad1e389b4b32d334d4e8c8fcc191afdb47056e6
Sander <Sandervanuum@hotmail.com>
2018-06-06 16:22:10 +0200

    fixed stuff
    added the needed refferences, removed the camera zoom logs

--- Assets/Scripts/Camera/CameraZoom.cs
+++ Assets/Scripts/Camera/CameraZoom.cs
@@ -16,19 +16,15 @@ public class CameraZoom : MonoBehaviour {
 	}
 		
 	void OnTriggerStay(Collider other){
-		Debug.Log ("Entered Trigger");
 		if (other.CompareTag (Constants.ZOOM_TAG)) {
-			Debug.Log ("In zoom trigger");
 			if (Input.GetButtonDown (Constants.PICKUP_BUTTON)){
 				if( mainCamera.enabled) {
-					Debug.Log ("Zoom in");
 					camFollow.enabled = false;
 					camCollision.enabled = false;
 					charMovement.enabled = false;
 					other.transform.GetChild(0).GetComponent<Camera>().enabled = true;
 					mainCamera.enabled = false;
 				} else if (!mainCamera.enabled){
-					Debug.Log ("Zoom out");
 					camFollow.enabled = true;
 					camCollision.enabled = true;
 					mainCamera.enabled = true;
