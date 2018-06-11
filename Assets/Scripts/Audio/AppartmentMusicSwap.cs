eec5cce26fffc7f0d7b1a336ae56c85a75fb589f
Chrisjan Kottelenberg <c.kottelenberg@gmail.com>
2018-06-11 13:19:30 +0200

    Added music and music changer to the appartment scene

---
+++ Assets/Scripts/Audio/AppartmentMusicSwap.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppartmentMusicSwap : MonoBehaviour {
+
+	public GameObject musicMuffledObject;
+	public GameObject musicHeadObject;
+
+    public AudioSource musicMuffled;
+    public AudioSource musicHead;
+
+    public static AppartmentMusicSwap instance;
+
+    private bool AppartmentMusic = true;
+
+    // Use this for initialization
+    void Awake () {
+        musicMuffledObject = GameObject.Find("MusicMuffled");
+        musicHeadObject = GameObject.Find("MusicHead");
+
+        musicMuffled = musicMuffledObject.GetComponent<AudioSource>();
+        musicHead = musicHeadObject.GetComponent<AudioSource>();
+
+        instance = this;
+    }
+
+    void Start()
+    {
+        musicHead.mute = true;
+    }
+
+	public void ChangeMusic()
+	{
+		//change music
+		
+		if (AppartmentMusic == true)
+		{
+            musicHead.mute = !musicHead.mute;
+            musicMuffled.mute = !musicHead.mute;
+            AppartmentMusic = false;
+		}
+		else
+		{
+            musicHead.mute = !musicHead.mute;
+            musicMuffled.mute = !musicHead.mute;
+            AppartmentMusic = true;
+		}
+
+	}
+}
