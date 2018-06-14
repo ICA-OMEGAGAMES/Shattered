8c05389d03a371d64685f73d23881e3362e955e7
Zoe Kroontje-Gillette <z.gillette@hotmail.com>
2018-06-14 13:51:21 +0200

    new intro and start sounds

--- Assets/Scripts/Main menu/LoadSceneOnClick.cs
+++ Assets/Scripts/Main menu/LoadSceneOnClick.cs
@@ -8,14 +8,23 @@ using UnityEngine.UI;
 public class LoadSceneOnClick : SceneTransition {
 
 	public int sceneIndex;
+	public AudioSource clipToPlayOnClick;
 
 	void Start()
 	{
 		GetComponent<Button>().onClick.AddListener(delegate {LoadByIndex(sceneIndex);});
+		clipToPlayOnClick = GetComponent<AudioSource> ();
 	}
 
 	public void LoadByIndex(int index)
 	{
+		StartCoroutine (PlayStartSound());
+
+	}
+
+	IEnumerator PlayStartSound(){
+		clipToPlayOnClick.Play ();
+		yield return new WaitForSeconds (.5f);
 		StartCoroutine (LoadingScreen());
 	}
 }
