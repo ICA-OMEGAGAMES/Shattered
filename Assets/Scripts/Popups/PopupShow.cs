0a5738ffdac1283f1d6507c4ee25d35ab85c2d0e
Zoe Kroontje-Gillette <z.gillette@hotmail.com>
2018-06-14 10:55:55 +0200

    popup setup

---
+++ Assets/Scripts/Popups/PopupShow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupShow : MonoBehaviour {
+
+	public GameObject popup; 
+
+	private bool popupShown = false;
+
+	void Start (){
+		popup.SetActive (false);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(!popupShown)
+			StartCoroutine (ShowPopup());
+	}
+
+	IEnumerator ShowPopup()
+	{
+		popupShown = true;
+		popup.SetActive (true);
+		yield return new WaitForSeconds(2);
+		popup.SetActive(false);
+	}
+}
